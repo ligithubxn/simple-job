0c69af7c09926e1cf2981cd2a534a8a1b1cd5f21
liws <liws>
2020-03-05 23:01:49 +0800

    11

--- src/main/java/com/simple/job/ExecuteJob.java
+++ src/main/java/com/simple/job/ExecuteJob.java
@@ -15,6 +15,7 @@ import org.slf4j.LoggerFactory;
 public class ExecuteJob implements Job {
     private final Logger logger = LoggerFactory.getLogger(getClass());
     public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+        logger.info("---- dev分支 ----");
         //instance有值，说明该节点服务器获得执行权限
         String instance = StorgeInfo.instance().getInstanceValue();
         if(StringUtils.isBlank(instance)) {
