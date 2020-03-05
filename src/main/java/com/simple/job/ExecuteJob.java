d5a54cdff7907ef02f68f3cd78732b29614a83ae
liws <liws>
2020-03-05 23:00:13 +0800

    11

--- src/main/java/com/simple/job/ExecuteJob.java
+++ src/main/java/com/simple/job/ExecuteJob.java
@@ -5,12 +5,15 @@ import org.apache.commons.lang3.StringUtils;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * 定时任务的基础类
  * Created by liws on 2017/11/28.
  */
 public class ExecuteJob implements Job {
+    private final Logger logger = LoggerFactory.getLogger(getClass());
     public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
         //instance有值，说明该节点服务器获得执行权限
         String instance = StorgeInfo.instance().getInstanceValue();
